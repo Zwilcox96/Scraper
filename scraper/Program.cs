eb4e01323831d92102d0e3dda1658fc8e28f1ec6
Amit Dahal <dahalamit4@gmail.com>
2019-03-14 11:24:55 -0700

    application can now grab near houses zestimate

--- scraper/Program.cs
+++ scraper/Program.cs
@@ -9,6 +9,9 @@ namespace scraper
     {
         static void Main(string[] args)
         {
+            //string infoArr = "hey this is Amit the greatest. Amit is coolest person i know. He rocks at everything.Amit is so cool yar yall dont even know.";
+           //FindString(infoArr);
+            
             //assume house info is given like this...
             int houseNumber = 1528;
             string streetname = "Hutchison Valley Dr";  //last term has to be abbrebiated(need to see if map api does that)
@@ -26,29 +29,40 @@ namespace scraper
             string html = client.DownloadString(zillowURL);
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(html);
-
-            
+          
             string AddressText = htmlDoc.DocumentNode.SelectSingleNode("//head/title").OuterHtml; //contains address embeded in title tag
             string roomsText = htmlDoc.DocumentNode.SelectSingleNode("//h3/span[2]").OuterHtml; //room info is embeded in this string
             string bathText = htmlDoc.DocumentNode.SelectSingleNode("//h3/span[4]").OuterHtml;
             string areaText = htmlDoc.DocumentNode.SelectSingleNode("//h3/span[6]").OuterHtml;
             string zestimateText = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'zestimate primary-quote')]/div").OuterHtml;
-            string solarFactorText = htmlDoc.DocumentNode.SelectSingleNode("(//div[@class ='home-details-facts-category-group-container'])[4]").OuterHtml;
 
-            Console.WriteLine(solarFactorText);
+
+            //couldnt do this :( zillow uses script for generating solarNumber and cant get past that
+            //string solarFactorText = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'truncated')]").OuterHtml;
+
+            /*
+             * AddRemainingHouses()
+             * in html file:
+             * 
+             * find: zsg-photo-card-price
+             * grab price by using $ to find recent index (zestimate, some zestimate values maynot be clean...)
+             * grab property by searching for: property-beds 
+             * grab address by using this: zsg-photo-card-address
+             * 
+             * 
+             */
+            
+            //Console.WriteLine(solarFactorText);
             //return numerical value of those fields
+            
             string houseAddress = getHouseAddress(AddressText);
             int numberOfBaths = getNumberOfBaths(bathText); 
             int numberOfBeds = getNumberOfBeds(roomsText);
             int areaInSqFt = getAreaInfo(areaText);
             int Zestimate = getZestimate(zestimateText);
 
-            Console.WriteLine("For house: " + houseAddress);
-            Console.WriteLine(areaInSqFt + " is area");
-            Console.WriteLine(numberOfBeds + " is number of beds");
-            Console.WriteLine(numberOfBaths + " is number of baths");
-            Console.WriteLine(Zestimate + " is the estimated price of home.");
 
+            FindString(html);
             Console.WriteLine("For house: " + house.houseAddress);
             Console.WriteLine(house.areaInSqFt + " is area");
             Console.WriteLine(house.numberOfBeds + " is number of beds");
@@ -57,6 +71,7 @@ namespace scraper
 
             Console.Read();
         }
+        
         static string GenerateURL(int houseNumber, string street,string city,string state, int zipcode, string lastTag)
         {
             string url = "https://www.zillow.com/homes/";
@@ -118,12 +133,44 @@ namespace scraper
 
         static WebClient SetHeaders(WebClient client)
         {
-            client.Headers[HttpRequestHeader.Accept] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+            client.Headers[HttpRequestHeader.Accept] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,q=0.8";
             client.Headers[HttpRequestHeader.AcceptEncoding] = "identity";
             client.Headers[HttpRequestHeader.AcceptLanguage] = "en-US,en;q=0.9";
             client.Headers[HttpRequestHeader.Upgrade] = "1";
             client.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36";
             return client;
         }
+
+        //this is to test indexOf method of string class
+        static void FindString(string htmlDOM)
+        {
+            int currentIndex = 0; //where we are within the arr of data
+            //zsg-photo-card-price">$
+            string relevantElement = "zsg-photo-card-price\">$"; //this substring(which is a class name) is used before providing relevatn housing info., stores the begining index
+            int relevantElementLength = relevantElement.Length; //indexOf returns the index where releevantElement is stored, the value we want comes after that strings
+            int relevantHouseInfoIndex = 0; //index which tracks where the relevant house index actally begins
+            string spanElement = "</span>";
+
+            do
+            {
+                relevantHouseInfoIndex = htmlDOM.IndexOf(relevantElement, currentIndex);
+                if(relevantHouseInfoIndex != -1)
+                {
+                    //find string which is zestimate that comes after relevant index
+                    //grab zestimate
+                    //grab bed
+                    int spanElementIndex = htmlDOM.IndexOf(spanElement, relevantHouseInfoIndex);
+                    int startIndex = relevantHouseInfoIndex + relevantElementLength;
+                    int HowManyNumbers = spanElementIndex - startIndex;
+                    string zestimate = htmlDOM.Substring(startIndex, HowManyNumbers);
+                    Console.WriteLine("zestimate is:!- " + zestimate);
+                    currentIndex = relevantHouseInfoIndex+1;
+                }
+            } while (relevantHouseInfoIndex != -1);
+            
+            Console.ReadLine();
+        }
+
     }
+
 }
